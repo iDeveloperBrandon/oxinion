e48acca973e8db14eae6c13f35bc7ea14b374dd7
iDeveloperBrandon <hsy0602@outlook.com>
2020-10-28 20:34:05 +0900

    working on form

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,21 @@
 const submitBtn = document.querySelector(".submit-btn");
 const displayForm = document.querySelector(".displayForm");
 const sideForm = document.querySelector(".side__form");
+const sideFormLocation = document.querySelector(".side__form__location");
+const sideFormDesc = document.querySelector(".side__form__description");
 
 displayForm.addEventListener("click", function () {
   sideForm.style.display = "block";
   document.querySelector(".createForm").style.display = "none";
 });
 
-submitBtn.addEventListener("submit", function (e) {
+submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log(123);
+  document.querySelector(".createForm").style.display = "block";
+  sideForm.style.display = "none";
+
+  const valueLocation = sideFormLocation.value;
+  const valueDesc = sideFormDesc.value;
+
+  console.log(valueLocation, valueDesc);
 });
