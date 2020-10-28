3669d764723a1fcefe2a4fa211b6845d18fe4d80
iDeveloperBrandon <hsy0602@outlook.com>
2020-10-28 20:53:12 +0900

    innerHTML

--- dist/app.js
+++ dist/app.js
@@ -3,6 +3,7 @@ const displayForm = document.querySelector(".displayForm");
 const sideForm = document.querySelector(".side__form");
 const sideFormLocation = document.querySelector(".side__form__location");
 const sideFormDesc = document.querySelector(".side__form__description");
+const posts = document.querySelector(".posts");
 
 displayForm.addEventListener("click", function () {
   sideForm.style.display = "block";
@@ -14,8 +15,20 @@ submitBtn.addEventListener("click", function (e) {
   document.querySelector(".createForm").style.display = "block";
   sideForm.style.display = "none";
 
-  const valueLocation = sideFormLocation.value;
-  const valueDesc = sideFormDesc.value;
+  let valueLocation = sideFormLocation.value;
+  let valueDesc = sideFormDesc.value;
 
   console.log(valueLocation, valueDesc);
+  posts.innerHTML = `
+  <article class="post">
+    <small><i class="fas fa-map-marker-alt"></i>Toronto, Canada</small>
+    <img src="" alt="example" />
+    <p>I want to be a developer in Canada. Can i?</p>
+    <hr />
+    <i class="far fa-heart"></i> 10
+    <i class="fas fa-stopwatch"></i> 10:04
+  </article>`;
+
+  sideFormLocation.value = "";
+  sideFormDesc.value = "";
 });
